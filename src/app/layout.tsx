7cf197513315b8c0a67a393cac9c74d9c5b58486
Gabriel Trajano <conversarcomgabriel@gmail.com>
2026-05-14 08:09:14 -0300

    Title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Spotify Charge Manager',
+  title: 'Spotify do Gabriel',
   description: 'Sistema programado por Gabriel Trajano',
 };
 
